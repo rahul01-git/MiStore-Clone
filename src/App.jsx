c4f68a57dfa350113349128d5c947bd58e72d3bc
rahul01-git <rahulneupane0090@gmail.com>
2023-05-30 17:16:22 +0545

    carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import './App.css'
-import Navbar from './components/Navbar'
-import PreNavbar from './components/PreNavbar'
-import { BrowserRouter as Router} from 'react-router-dom'
+import "./App.css";
+import Navbar from "./components/Navbar";
+import PreNavbar from "./components/PreNavbar";
+import { BrowserRouter as Router } from "react-router-dom";
+import Slider from "./components/Slider";
+import { banner } from "./data/data.json";
 
 export default function App() {
   return (
     <Router>
-      <PreNavbar/>
-      <Navbar/>
+      <PreNavbar />
+      <Navbar />
+      <Slider start={banner.start} />
     </Router>
-  )
+  );
 }
---
+++ src/components/Slider.jsx
@@ -0,0 +1,15 @@
+import { Carousel } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css' 
+const Slider = ({ start }) => {
+  return (
+    <Carousel fade>
+      {start.map((item,idx) => (
+        <Carousel.Item key={idx}>
+          <img src={item} className="d-block w-100" alt="item" />
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
+};
+
+export default Slider;
